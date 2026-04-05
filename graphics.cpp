7bb6a1c4bfb6e2acd984aff267e50ae89ae37763
Сахно Виктория <victoriassnnnn@gmail.com>
2026-04-05 17:07:26 +0300

    Теперь есть страшненькая анимация

--- graphics.cpp
+++ graphics.cpp
@@ -4,50 +4,105 @@
 #include <iostream>
 using std::vector;
 
-class Point
+struct Point 
+{ 
+    double x, y;
+    Point(double x, double y) : x(x), y(y) {}
+};
+
+class Graphic_object
 {
-private:
-    double value_x, value_y;
+protected:
+    sf::Color color;
+    double scale = 1e-9;
+    Point Centre{ 600, 300 }; //центр окна
 public:
-    Point(double x, double y) : value_x(x), value_y(y) {}
-    void Draw(Point& point, double scale, sf::RenderWindow& window, double x_sun, double y_sun) //метод отрисовки каждой точки траектории 
+    Graphic_object () {}
+    virtual void Draw(sf::RenderWindow& window) = 0; //метод отрисовки траектории 
+};
+
+class Sun : public Graphic_object
+{
+public:
+    Sun() { color = sf::Color::Yellow; }
+    void Draw(sf::RenderWindow& window)
+    {
+        sf::CircleShape Sun(10);
+        Sun.setOrigin(sf::Vector2f(10, 10));
+        Sun.setPosition(sf::Vector2f(Centre.x, Centre.y));
+        Sun.setFillColor(color);
+        window.draw(Sun);
+    }
+};
+
+class Planet : public Graphic_object
+{
+protected:
+    sf::Time pause = sf::milliseconds(10);
+    vector<Point> trajectory;
+    sf::Clock animation_clock;
+    int current_index = 0; //хранит текущее положение (текущую точку)
+public:
+    Planet(std::ifstream& file)
+    {
+        std::string header;
+        std::getline(file, header); //пропускаем заголовок
+        double t, x, y;
+        char comma;
+        while (file >> t >> comma >> x >> comma >> y)
+        {
+            Point point(x, y);
+            trajectory.push_back(point);
+        }
+        file.close();
+    }
+    unsigned int Get_trajectory() { return trajectory.size(); }
+    void Update()
+    {
+        if (animation_clock.getElapsedTime() >= pause)
+        {
+            current_index = (current_index + 1) % trajectory.size();
+            animation_clock.restart();
+        }
+    }
+    void Draw(sf::RenderWindow& window)
     {
         sf::CircleShape p(3);
-        p.setFillColor(sf::Color::Blue);
-        p.setPosition(sf::Vector2f(point.value_x*scale + x_sun, point.value_y*scale + y_sun));
+        p.setFillColor(color);
+        p.setPosition(sf::Vector2f(trajectory[current_index].x * scale + Centre.x, trajectory[current_index].y * scale + Centre.y));
         p.setOrigin(3, 3);
         window.draw(p);
     }
 };
 
+class Earth : public Planet
+{
+public:
+    Earth(std::ifstream& file) : Planet(file) { color = sf::Color::Blue; }
+};
+
+class Mars : public Planet
+{
+public:
+    Mars(std::ifstream& file) : Planet(file) { color = sf::Color::Red; }
+};
+
+
 int main()
 {
+    setlocale(LC_ALL, "Russian");
 
-    vector<Point> points;
-    std::ifstream file("C:\\Users\\1\\source\\repos\\SPACE\\data\\earth_orbit.csv");
+    std::ifstream file1("C:\\Users\\1\\source\\repos\\SPACE\\data\\earth_orbit.csv");
+    if (!file1.is_open()) { std::cout << "Не удалось открыть file1"; }
 
-    if (!file.is_open()) { std::cout << "Open error" << std::endl; }
+    std::ifstream file2("C:\\Users\\1\\source\\repos\\SPACE\\data\\mars_orbit.csv");
+    if (!file2.is_open()) { std::cout << "Не удалось открыть file2"; }
 
-    std::string header;
-    std::getline(file, header);
-
-    double t, x, y;
-    char comma;
-    while (file >> t >> comma >> x >> comma >> y)
-    {
-        Point point(x, y);
-        points.push_back(point);
-    }
-    file.close();
-    std::cout << "Points: " << points.size() << std::endl; //загружено точек
+    Earth earth(file1);
+    Mars mars(file2);
+    Sun sun;
 
     sf::RenderWindow window(sf::VideoMode(1200, 600), "Orbits");
-    window.clear(sf::Color::White);
-    sf::CircleShape Sun(10);
-    Sun.setOrigin(sf::Vector2f(10, 10));
-    Sun.setPosition(sf::Vector2f(600, 300));
-    Sun.setFillColor(sf::Color::Yellow);
-
     while (window.isOpen())
     {
         sf::Event event;
@@ -56,11 +111,19 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        window.clear();
-        window.draw(Sun);
-        for (int i = 0; i < points.size(); i++) { points[i].Draw(points[i], 1e-9, window, 600, 300); }
+
+        sun.Draw(window);
+
+        for (unsigned int i = 0; i < earth.Get_trajectory(); i++)
+        {
+            earth.Draw(window);
+            mars.Draw(window);
+            earth.Update();
+            mars.Update();
+        }
         window.display();
     }
 
+    
     return 0;
 }
